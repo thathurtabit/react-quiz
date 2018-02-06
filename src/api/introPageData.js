52e915099e539353f1a1343425fffa484a1fc013
hellointerlude <hellointerlude@gmail.com>
2018-02-06 18:52:49 +0000

    Update introPageData.js
    Sick copy

--- src/api/introPageData.js
+++ src/api/introPageData.js
@@ -1,6 +1,6 @@
 const introPageData = {
-  p1: "Let’s face it, design is a broad church.",
-  p2: "<strong>As a designer, there are many processes,</strong> philosophies, companies and careers for you to explore, but whichever path you choose to take, understanding what makes you tick and how to work best with other people is likely to have a big influence on how successful you will be.",
+  p1: "Design is a broad church.",
+  p2: "As a designer, there are many processes, philosophies, companies and careers for you to explore. Whichever path you choose to take, understanding what makes you tick and how to work best with other people is going to have a big influence on how successful you will be.",
   p3: "While it’s really just for fun, this short quiz might help you work some of that out.",
 }
 
